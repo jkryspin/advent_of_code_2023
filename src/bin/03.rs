7c65c099a49e59e5cc25eb678ca8725b37774743
John Kryspin <johnkryspin@gmail.com>
2023-12-03 15:00:52 -0500

    boom day 3 done

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,24 +1,9 @@
-use std::ops::Add;
+use std::collections::{HashMap, HashSet};
+use std::ops::{Add, DerefMut};
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let lines = input.lines().collect::<Vec<_>>();
-    let width = lines.clone().first().unwrap().len();
-    let height = lines.len();
-    let mut grid_raw = vec!['.'; width * height];
-
-    // Vector of 'width' elements slices
-    let mut grid_base: Vec<_> = grid_raw.as_mut_slice().chunks_mut(width).collect();
-
-    // Final 2d array `&mut [&mut [_]]`
-    let grid = grid_base.as_mut_slice();
-
-    // Accessing data
-    for (y, l) in lines.iter().enumerate() {
-        for (x, c) in l.chars().enumerate() {
-            grid[y][x] = c;
-        }
-    }
+    let grid = create_grid(input);
     let mut sum = 0;
     grid.iter().enumerate().for_each(|(y, l)| {
         let mut i = 0;
@@ -26,7 +11,9 @@ pub fn part_one(input: &str) -> Option<u32> {
             let mut number_s = "".to_string();
             let mut c = l[i];
             let start = (i, y);
+            let mut end = (i, y);
             while c.is_ascii_digit() {
+                end = (i, y);
                 number_s.push_str(&c.to_string());
                 if let Some(&x) = l.get(i + 1) {
                     i += 1;
@@ -35,12 +22,10 @@ pub fn part_one(input: &str) -> Option<u32> {
                     break;
                 }
             }
-            let end = (i, y);
             i += 1;
             if number_s.len() > 0 {
-                println!("{}", number_s);
                 let positions = positions(start, end);
-                if adjacent_to_symbol(positions, grid.as_ref()) {
+                if adjacent_to_symbol(positions, &grid) {
                     sum += number_s.parse::<u32>().unwrap();
                 }
             }
@@ -55,7 +40,32 @@ fn positions(start: (usize, usize), end: (usize, usize)) -> Vec<(usize, usize)> 
         .map(|x| (x, start.1))
         .collect();
 }
-fn adjacent_to_symbol(positions: Vec<(usize, usize)>, grid: &[&mut [char]]) -> bool {
+
+fn create_grid(input: &str) -> Vec<Vec<char>> {
+    let lines = input.lines().collect::<Vec<_>>();
+    let width = lines.clone().first().unwrap().len();
+    let height = lines.len();
+    let mut grid_raw = vec!['.'; width * height];
+
+    // Vector of 'width' elements slices
+    let grid_base: Vec<_> = grid_raw
+        .chunks_mut(width)
+        .into_iter()
+        .map(|x| x.to_vec())
+        .collect();
+
+    // Final 2d array `&mut [&mut [_]]`
+    let mut grid = grid_base;
+
+    // Accessing data
+    for (y, l) in lines.iter().enumerate() {
+        for (x, c) in l.chars().enumerate() {
+            grid[y][x] = c;
+        }
+    }
+    return grid;
+}
+fn adjacent_to_symbol(positions: Vec<(usize, usize)>, grid: &Vec<Vec<char>>) -> bool {
     for p in positions.iter() {
         for a in -1i32..=1 {
             for b in -1i32..=1 {
@@ -76,7 +86,70 @@ fn adjacent_to_symbol(positions: Vec<(usize, usize)>, grid: &[&mut [char]]) -> b
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = create_grid(input);
+    let mut cog_map = HashMap::<(usize, usize), Vec<u32>>::new();
+    grid.iter().enumerate().for_each(|(y, l)| {
+        let mut i = 0;
+        while i < l.len() {
+            let mut number_s = "".to_string();
+            let mut c = l[i];
+            let start = (i, y);
+            let mut end = (i, y);
+            while c.is_ascii_digit() {
+                end = (i, y);
+                number_s.push_str(&c.to_string());
+                if let Some(&x) = l.get(i + 1) {
+                    i += 1;
+                    c = x;
+                } else {
+                    break;
+                }
+            }
+            i += 1;
+            if number_s.len() > 0 {
+                let positions = positions(start, end);
+                let cog_positions = cog_positions(positions, &grid);
+                cog_positions.iter().for_each(|(x_i, y_i)| {
+                    if let Some(m) = cog_map.get_mut(&(x_i.clone(), y_i.clone())) {
+                        m.push(number_s.parse().unwrap());
+                    } else {
+                        cog_map.insert((x_i.clone(), y_i.clone()), vec![number_s.parse().unwrap()]);
+                    }
+                })
+            }
+        }
+    });
+
+    let mut sum = 0;
+    cog_map.iter().for_each(|(key, val)| {
+        if val.len() == 2 {
+            sum += (val[0] * val[1]);
+        }
+    });
+
+    Some(sum)
+}
+fn cog_positions(positions: Vec<(usize, usize)>, grid: &Vec<Vec<char>>) -> HashSet<(usize, usize)> {
+    let mut found_positions = HashSet::<(usize, usize)>::new();
+    for p in positions.iter() {
+        for a in -1i32..=1 {
+            for b in -1i32..=1 {
+                if a == 0 && b == 0 {
+                    continue;
+                }
+                let y_pos = p.1 as i32 + a;
+                let x_pos = p.0 as i32 + b;
+                if let Some(y) = grid.get((y_pos) as usize) {
+                    if let Some(x) = y.get(x_pos as usize) {
+                        if x == &'*' {
+                            found_positions.insert((x_pos as usize, y_pos as usize));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return found_positions;
 }
 
 #[cfg(test)]
@@ -111,7 +184,7 @@ mod tests {
     #[test]
     fn test_part_one_help_2() {
         let result = part_one(
-        "12.......*..
+            "12.......*..
 +.........34
 .......-12..
 ..78........
@@ -122,13 +195,29 @@ mod tests {
 ............
 2.2......12.
 .*.........*
-1.1..503+.56");
+1.1..503+.56",
+        );
         assert_eq!(result, Some(925));
+    }
+
+    #[test]
+    fn test_part_one_help_3() {
+        let result = part_one(
+            "100
+200",
+        );
+        assert_eq!(result, Some(0));
+    }
+
+    #[test]
+    fn test_part_one_help_4() {
+        let result = part_one("503+");
+        assert_eq!(result, Some(503));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
